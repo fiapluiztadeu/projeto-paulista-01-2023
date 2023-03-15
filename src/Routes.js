f9b39a54827860fc93ff3dcbc6f0c17d1182e8b0
fiapluiztadeu <pf1665@fiap.com.br>
2023-03-14 21:15:31 -0300

    2

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,7 @@ import Sobre from './paginas/sobre';
 import Cep from "./paginas/cep";
 import Home from "./paginas/home";
 
-// imposts
+// imposts fafa
 export default function Routes(){
     return(
         <Switch>
